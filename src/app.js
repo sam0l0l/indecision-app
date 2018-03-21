333bba2aed9edd95e8113c1a1bc6687f279dd918
Sam Morris <sam@popchart.family>
2018-03-21 00:52:59 -0700

    saving and loading options data

--- src/app.js
+++ src/app.js
@@ -10,10 +10,22 @@ class IndecisionApp extends React.Component {
         }
     }
     componentDidMount(){
-        console.log('fetching data');
+        try{
+            const json = localStorage.getItem('options');
+            const options = JSON.parse(json);
+    
+            if(options){
+                this.setState(() => ({options: options}));
+            }
+        }catch(e){
+            // Do nothing
+        } 
     }
     componentDidUpdate(prevProps, prevState){
-        console.log('saving data');
+        if(prevState.options.length !== this.state.options.length){
+            const json = JSON.stringify(this.state.options);
+            localStorage.setItem('options', json);
+        }  
     }
     componentWillUnmount(){
         console.log('componentWillUnmount');
@@ -94,6 +106,7 @@ const Options = (props) => {
     return (
         <div>
             <button onClick={props.handleDeleteOptions}>Remove All</button>
+            {props.opts.length === 0 && <p>Please add an option to get started.</p>}
             {
                 props.opts.map((option) => (
                     <Option 
@@ -135,6 +148,10 @@ class AddOption extends React.Component {
         const error = this.props.handleAddOption(option);
 
         this.setState(() => ({ error: error }))
+
+        if(!error){
+            e.target.elements.option.value = '';
+        }
     }
     render(){
         return (
