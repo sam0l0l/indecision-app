918fda2edccd8e16eb13a1246605e37e47d4c740
Sam Morris <sam@popchart.family>
2018-03-24 16:11:57 -0700

    es6 import and export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
+//import './utils.js';
+/* import { square, add } from './utils.js';
+
 console.log('app.js is running!!farts');
+console.log(square(3));
+console.log(add(1, 2)); */
+
+import { isAdult, canDrink } from './person.js';
+
+console.log(isAdult(17));
+console.log(isAdult(19));
+console.log(canDrink(20));
+console.log(canDrink(23));
