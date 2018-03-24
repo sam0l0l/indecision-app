503f58e403482757115286021fc68dba3cb1315e
Sam Morris <sam@popchart.family>
2018-03-24 16:31:06 -0700

    default exports

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 //import './utils.js';
-/* import { square, add } from './utils.js';
+/*  import subtract, { square, add } from './utils.js';
 
 console.log('app.js is running!!farts');
 console.log(square(3));
-console.log(add(1, 2)); */
+console.log(add(1, 2)); 
+console.log(subtract(100, 2));  */
 
-import { isAdult, canDrink } from './person.js';
+import isSenior, { isAdult, canDrink } from './person.js';
 
 console.log(isAdult(17));
-console.log(isAdult(19));
-console.log(canDrink(20));
 console.log(canDrink(23));
+
+console.log(isSenior(64));
