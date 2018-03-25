b75fdd6916a7073824f128d067329c81ff947759
Sam Morris <sam@popchart.family>
2018-03-24 18:46:57 -0700

    one component per file

--- src/app.js
+++ src/app.js
@@ -1,25 +1,5 @@
-//import './utils.js';
-/*  import subtract, { square, add } from './utils.js';
-
-console.log('app.js is running!!farts');
-console.log(square(3));
-console.log(add(1, 2)); 
-console.log(subtract(100, 2));  */
-
-/* import isSenior, { isAdult, canDrink } from './person.js';
-
-console.log(isAdult(17));
-console.log(canDrink(23));
-
-console.log(isSenior(64)); */
-/* 
-import validator from 'validator';
-
-console.log(validator.isEmail('hi@sam.virallo.vin')); */
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-const template = <p>yada yada this is jsx</p>;
-ReactDOM.render(template, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
