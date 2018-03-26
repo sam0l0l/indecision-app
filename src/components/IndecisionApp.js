df7fb0a10487744616a982436fd122699c1bf4e2
Sam Morris <sam@popchart.family>
2018-03-25 18:04:49 -0700

    themeing with variables

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -64,17 +64,19 @@ export default class IndecisionApp extends React.Component {
         return (
             <div>
                 <Header subtitle={subtitle} />
-                <Action 
-                handlePick={this.handlePick}
-                hasOptions={this.state.options.length > 0} />
-                <Options 
-                opts={this.state.options}
-                handleDeleteOptions={this.handleDeleteOptions}
-                handleDeleteOption={this.handleDeleteOption}
-                />
-                <AddOption 
-                    handleAddOption={this.handleAddOption}
-                />
+                <div className="container">
+                    <Action 
+                    handlePick={this.handlePick}
+                    hasOptions={this.state.options.length > 0} />
+                    <Options 
+                    opts={this.state.options}
+                    handleDeleteOptions={this.handleDeleteOptions}
+                    handleDeleteOption={this.handleDeleteOption}
+                    />
+                    <AddOption 
+                        handleAddOption={this.handleAddOption}
+                    />
+                </div>
                 <OptionModal 
                     handleClearSelectedOption={this.handleClearSelectedOption}
                     selectedOption={this.state.selectedOption}
